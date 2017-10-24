10b440e7b056ff026a83c8723e7770638578fbc8
Unknown <dimaushenko@mail.ru>
2017-10-25 00:06:49 +0300

    move old project

---
+++ core/src/by/kutovoy/satellite/positions/EntityPosition.java
@@ -0,0 +1,21 @@
+package by.kutovoy.satellite.positions;
+
+    
+/**
+ *
+ * @author dmitry
+ */
+public class EntityPosition {
+    
+    private int x;
+    private int y;
+    
+    public EntityPosition(){
+        this.x = 0;
+        this.y = 0;
+    }
+    public EntityPosition(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ desktop/src/by/kutovoy/satellite/desktop/enums/Other.java
@@ -0,0 +1,55 @@
+package by.kutovoy.satellite.desktop.enums;
+
+/**
+ *
+ * @author dmitry
+ */
+public class Other {
+
+    /**
+     * @return the BTN_WIDTH
+     */
+    public static int getBTN_WIDTH() {
+        return BTN_WIDTH;
+    }
+
+    /**
+     * @return the BTN_HEIGHT
+     */
+    public static int getBTN_HEIGHT() {
+        return BTN_HEIGHT;
+    }
+
+    /**
+     * @return the X_STEP
+     */
+    public static int getX_STEP() {
+        return X_STEP;
+    }
+
+    /**
+     * @return the Y_STEP
+     */
+    public static int getY_STEP() {
+        return Y_STEP;
+    }
+
+    /**
+     * @return the SCALE_STEP
+     */
+    public static double getSCALE_STEP() {
+        return SCALE_STEP;
+    }
+
+    private Other(){}
+    
+    private static final int BTN_WIDTH = 42;
+    private static final int BTN_HEIGHT = 42;
+    
+    private static final int X_STEP = 50;
+    private static final int Y_STEP = 50;
+    
+    private static final double SCALE_STEP = 0.1;
+    
+    
+}
